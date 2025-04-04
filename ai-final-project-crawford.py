f1a77a3ea0f30d032fdfa90090decbd35a24dce3
jillcrawford <jrcrawford@anderson.edu>
2025-04-04 14:14:04 -0400

    building out puzzle from web

--- ai-final-project-crawford.py
+++ ai-final-project-crawford.py
@@ -9,3 +9,18 @@ url = 'https://nine.websudoku.com/?level=1'
 web = requests.get(url)
 soup = BeautifulSoup(web.text, 'html.parser')
 
+# filling in the puzzle with the info from the web
+puzzle = []
+# 9 x 9 grid
+for row in range(9):
+    cur_row = []
+    for col in range(9):
+        cellnum = (row)(col)
+        cell = soup.find('input', id=cellnum)
+        value = cell.get('value') if cell else 0
+        cur_row.append(int(value))
+    puzzle.append(cur_row)
+
+# printing puzzle out
+for row in puzzle:
+    print(row)
