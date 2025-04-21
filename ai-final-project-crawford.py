68fb4b6b3b3397eeb21b15b92899a36fe22074f3
jillcrawford <jrcrawford@anderson.edu>
2025-04-21 19:10:23 -0400

    fixed syntax of backtracking function

--- ai-final-project-crawford.py
+++ ai-final-project-crawford.py
@@ -65,13 +65,13 @@ def fill_obvious(board):
 def backtracking(board):
     for row in range(9):
         for col in range(9):
-        if board[row][col] == 0:
-            for num in range(1, 10):
-                if is_valid(board, row, col, num):
-                    board[row][col] = num
-                    if backtracking(board):
-                        return True
-                    board[row][col] = 0
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if backtracking(board):
+                            return True
+                        board[row][col] = 0
                 return False
     return True
 
